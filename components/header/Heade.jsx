4b21b882be8d7d0bbf33806b255f0ede689eb3a6
najib2050 <abdiwelinoor2@gmail.com>
2023-09-17 08:45:28 +0300

    componet links created

---
+++ components/header/Heade.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Heade = () => {
+  return (
+    <div>Heade
+        <nav>
+            <a src="/">logo</a>
+            <ul>
+                <li>
+                    <a href="/posts">posts</a>
+                </li>
+                <li>
+                    <a href="/new-post">new post</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Heade
--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// import App from './App.jsx'
+// import './index.css'
+
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Heade from '../components/header/Heade.jsx';
+
+
 
 const router= createBrowserRouter([
   {
     path: "/",
-    element:<div>hello world!</div>,
+    element:<Heade/>,
     errorElement:<h1>erro the page doest exist 404</h1>
 
   },
   {
-    path:'/about',
-    element:<div>about page</div>
+    path:'/posts',
+    element:<div>posts page</div>
   }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router}/>
-    <App />
+    {/* <App /> */}
   </React.StrictMode>,
 )
