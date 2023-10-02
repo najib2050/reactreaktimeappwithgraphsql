fa226b48416df7e2f0e2129648df0afa5d9096ae
najib2050 <abdiwelinoor2@gmail.com>
2023-10-02 08:48:52 +0300

    read graphql queries with useQuery hook

---
+++ components/post/Post.jsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+
+
+
+const Post = (props) => {
+    console.log(props)
+  return (
+    <div>post</div>
+  )
+}
+
+export default Post
--- components/posts/Posts.jsx
+++ components/posts/Posts.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import { gql, useQuery } from '@apollo/client'
+import Post from '../post/Post';
+import { Grid } from './style';
+// import React from 'react'
 
+/*
+how to write query expression
+use gql tag
+ */
+
+  /*
+    how do we intract with qragh sql 
+     we can intract with by using these terminolgies
+       1:useMutation hook
+         -is use for DELETE, UPDATE AND INSERT
+       2:useQuerry hook
+         - is use for READ
+       3:useSubscribtion hook
+         - is use for REAL TIME DATA
+    */
+const GetAllPost = gql`
+  query MyQuery {
+    posts {
+      body
+      date
+      id
+      thumnail
+      title
+    }
+  }
+
+
+`;
 const Posts = () => {
+  const { loading, error, data } = useQuery(GetAllPost)
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
+  console.log("data", data)
   return (
-    <div>Posts</div>
+    <Grid>
+      {data.posts.map((post)=>{
+        return <Post {...post} key={post.id}/> 
+      })}
+    </Grid>
+    
   )
 }
 
