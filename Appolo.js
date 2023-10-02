455a5c98cb7a3604e9034f2bbad8d41c3017eda1
najib2050 <abdiwelinoor2@gmail.com>
2023-10-02 10:44:01 +0300

    fore correction

--- Appolo.js
+++ Appolo.js
@@ -1,17 +1,31 @@
 
-import {ApolloClient, HttpLink, InMemoryCache} from "@apollo/client";
+import {ApolloClient,  InMemoryCache} from "@apollo/client";
  
-const clienmtLink=new HttpLink({
-    // uri specifies the URL of our GraphQL server
-    url:"https://proper-iguana-40.hasura.app/v1/graphql",
-    // url:import.meta.VIT_HASURA_URL N ,
-    // cache is an instance of InMemoryCache, which Apollo Client uses to cache query results after fetching them.
-    headr:{
-       " x-hasura-admin-secret":'qnNq2zCC0SPbeuLQ1gjoyFyKM1lSPv3FtG3uS5HjLDm93u2fOH2fZNrGxVBNyzjR'
-    }
-})
+// const clientLink=new HttpLink({
+//     // uri specifies the URL of our GraphQL server
+//     uri:"https://proper-iguana-40.hasura.app/v1/graphql",
+//     //  uri :import.meta.env.VITE_HASURA_URL,
+//     // cache is an instance of InMemoryCache, which Apollo Client uses to cache query results after fetching them.
+//     headers:{
+//     //   " x-hasura-admin-secret":import.meta.env.VITE_SECRET
+//       " x-hasura-admin-secret":"qnNq2zCC0SPbeuLQ1gjoyFyKM1lSPv3FtG3uS5HjLDm93u2fOH2fZNrGxVBNyzjR",
+//        Accept: "application/json",
+//       "Content-Type": "application/json"
+//     }
+// })
 
 export const client=new ApolloClient({
-    link:clienmtLink,
+    // link:clientLink,
+    uri:"https://proper-iguana-40.hasura.app/v1/graphql",
+    /* OR 
+            uri specifies the URL of our GraphQL server
+    */
+    // uri :import.meta.env.VITE_HASURA_URL,
+    headers:{
+      "x-hasura-admin-secret":"qnNq2zCC0SPbeuLQ1gjoyFyKM1lSPv3FtG3uS5HjLDm93u2fOH2fZNrGxVBNyzjR",
+        /* OR */
+        // " x-hasura-admin-secret":import.meta.env.VITE_SECRET
+          },
+          // cache is an instance of InMemoryCache, which Apollo Client uses to cache query results after fetching them.
     cache:new InMemoryCache()
 })
