7ed70a7cabec2f9e02d06492e1fcbfcfb8c553e9
najib2050 <abdiwelinoor2@gmail.com>
2023-10-03 22:18:18 +0300

    using id send a request to db gql then return data

--- components/postInfo/PostInfo.jsx
+++ components/postInfo/PostInfo.jsx
@@ -1,8 +1,59 @@
+import { gql, useQuery } from '@apollo/client'
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import { Thumnail } from '../post/Style'
+
+// create query object 
+const Get_Single_posq_Query=gql`
+query MyQuery($id:Int) {
+  posts(where: {id: {_eq:$id}}) {
+    title
+    body
+    date
+    id
+    thumnail
+  }
+}
+`
 
 function PostInfo() {
+  //to get id on the prowser use useParams hook by tracking the id
+  /*destracture it further 
+  instead of  console.log(id.id) use \
+  this const {id}=useParams()
+  */
+  const {id}=useParams()
+  console.log(id)
+  //call querry object above by using useQuerry HOOK object
+  const { loading, error, data } = useQuery(Get_Single_posq_Query, {
+    /* 
+    The variables option is an object that contains all of the variables we want to pass to our GraphQL query. 
+    In this case, we want to pass the currently selected id from the dropdown.
+    */
+    variables: { id:id },
+  });
+  
+  if (loading) return null;
+  if (error) return `Error! ${error}`;
+  console.log(data)
+
+
+
   return (
-    <div>PostInfo</div>
+    // display the data
+    <div>
+         {data.posts.map((post,index)=>{
+          return <>
+          <ul key={post.id}>
+            <p>{index}</p>
+            <li>{post.title}</li>
+            <li>{new Date().toDateString(post.date)}</li>
+            <li>{post.id}</li>
+            <Thumnail src={post.thumnail} alt=''/>
+          </ul>
+          </>
+         })}
+    </div>
   )
 }
 
